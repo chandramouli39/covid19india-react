bcea4e41e606995f931ffd04a4aa587ce805ddac
chandramouli39 <chandramouli39@users.noreply.github.com>
2021-03-27 03:59:56 +0000

    Deploying to gh-pages from  @ c907eeceef0e9d7ca390e4a1294d5da824c8b488 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8bfca1f42569c06c301089024e81f2bd.js"
+  "/precache-manifest.cf6a30577a2805e5379218acca038e7f.js"
 );
 
 self.addEventListener('message', (event) => {
